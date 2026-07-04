c230c0d747f636cbd0a107be647a3592edebf953
Bluey <tairaerick.com>
2026-07-04 14:54:22 -0300

    a

--- include/ResourceManager.hpp
+++ include/ResourceManager.hpp
@@ -4,12 +4,14 @@
 #include <vector>
 #include <string>
 #include <tuple>
+#include "Process.hpp"
 
 class ResourceManager{
 	private:
 	
 	int disk_blocks;
 	int occupied_segments;
+	std::vector<int> disk;
 	std::vector<std::tuple<char, int, int>> occupied_segments_instructions;
 	std::vector<std::tuple<int, int, char, int>> process_instructions;
 	
@@ -17,16 +19,23 @@ class ResourceManager{
 	// Recebe o file "files.txt"e ocupa as variáveis
 	ResourceManager(const std::string& file);
 	
+	// Getters 
 	int getDiskBlocks() const { return disk_blocks; }
 	
     int getOccupiedSegments() const { return occupied_segments; }
-        
+    
+    std::vector<int> getDisk() { return disk; }
+    
     const std::vector<std::tuple<char, int, int>>& getOccupiedSegmentsInstructions() const { 
     	 return occupied_segments_instructions; 
     }
     const std::vector<std::tuple<int, int, char, int>>& getProcessInstructions() const { 
     	 return process_instructions; 
     }
+    
+    // Executa a instrução de arquivo do respectivo processo
+    void execInstruction(Process* p);
+    
 };
 
 #endif
