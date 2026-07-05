39ee763a7b242d999c0b892b02ee0621062db24c
Bluey <tairaerick.com>
2026-07-04 22:09:43 -0300

    File System completo

--- include/ResourceManager.hpp
+++ include/ResourceManager.hpp
@@ -1,19 +1,22 @@
 #ifndef RESOURCEMANAGER_HPP
 #define RESOURCEMANAGER_HPP
 
+#include <bitset>
 #include <vector>
 #include <string>
 #include <tuple>
+#include <string>
 #include "Process.hpp"
 
 class ResourceManager{
 	private:
 	
-	int disk_blocks;
-	int occupied_segments;
-	std::vector<int> disk;
-	std::vector<std::tuple<char, int, int>> occupied_segments_instructions;
-	std::vector<std::tuple<int, int, char, int>> process_instructions;
+	int disk_blocks;														// Total de blocos
+	int occupied_segments;													// Segmentos ocupados
+	std::bitset<1000> bit_map;												// Mapa de bits
+	std::vector<char> disk;													// Disco				
+	std::vector<std::tuple<char, int, int>> occupied_segments_instructions; // Instruções da ocupação dos segmentos
+	std::vector<std::tuple<int, int, char, int>> process_instructions;      // Instruções dos processos
 	
 	public:
 	// Recebe o file "files.txt"e ocupa as variáveis
@@ -24,17 +27,26 @@ class ResourceManager{
 	
     int getOccupiedSegments() const { return occupied_segments; }
     
-    std::vector<int> getDisk() { return disk; }
+    std::vector<char> getDisk() { return disk; }
+    
+    std::bitset<1000> getBitMap() { return bit_map; }
     
     const std::vector<std::tuple<char, int, int>>& getOccupiedSegmentsInstructions() const { 
     	 return occupied_segments_instructions; 
     }
+    
     const std::vector<std::tuple<int, int, char, int>>& getProcessInstructions() const { 
     	 return process_instructions; 
     }
     
+    void execSegmentsInstruction();
+    
     // Executa a instrução de arquivo do respectivo processo
-    void execInstruction(Process* p);
+    void execProcessInstruction(Process* p);
+    
+    void execAllProcessInstructions(const std::vector<Process*>& processes); 
+    
+    std::vector<std::string> file_system_log;
     
 };
 
